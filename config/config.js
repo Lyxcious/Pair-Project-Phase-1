6c3189597e0aeb68f95708cabba65a3c66080c22
basyri <husain.basyri@gmail.com>
2019-05-24 06:02:43 +0700

    update

--- config/config.js
+++ config/config.js
@@ -1,16 +1,18 @@
-require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
+require('dotenv').config({
+  silent: process.env.NODE_ENV === 'production'
+});
 
 module.exports = {
- "development": {
-   "username": process.env.DATABASE_USERNAME,
-   "password": process.env.DATABASE_PASSWORD,
-   "database": process.env.DATABASE_NAME,
-   "host": process.env.DATABASE_URL,
-   "dialect": "postgres"
- },
- "production": {
-   "use_env_variable": "DATABASE_URL",
-   "url": process.env.DATABASE_URL,
-   "dialect": "postgres"
- }
+  "development": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "ShopNShop",
+    "host": "localhost",
+    "dialect": "postgres"
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL",
+    "url": process.env.DATABASE_URL,
+    "dialect": "postgres"
+  }
 }
